64c73e3afbab791772c2310e4030899062958959
mahmudmhb1 <mahmud.mhb1@gmail.com>
2024-04-02 15:56:01 +0600

    update url

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json())
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: '*',
   credentials: true
 }))
 app.use(cookieParser())
